426835c5473647d3923fb9e1a870c1b721a95466
Alex Zhukovich <zhukovich.a@gmail.com>
2018-04-15 12:19:12 +0200

    Updated .gitignore file

--- app/src/main/java/com/alexzh/tutorial/notificationdemo/DetailActivity.java
+++ app/src/main/java/com/alexzh/tutorial/notificationdemo/DetailActivity.java
@@ -1,8 +1,8 @@
 package com.alexzh.tutorial.notificationdemo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class DetailActivity extends ActionBarActivity {
+public class DetailActivity extends AppCompatActivity {
 
     public static final String TEXT_MESSAGE = "text_message";
 
