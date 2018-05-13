d4235cc61c01f249f841a9ca61bf9f9ae7449816
Alex Zhukovich <zhukovich.a@gmail.com>
2018-05-13 12:16:57 +0200

    Removed fragment_detail.xml and PlaceholderFragment for simplification of the demo. Updated activity_detail layout.

--- app/src/main/java/com/alexzh/tutorial/notificationdemo/DetailActivity.java
+++ app/src/main/java/com/alexzh/tutorial/notificationdemo/DetailActivity.java
@@ -1,15 +1,9 @@
 package com.alexzh.tutorial.notificationdemo;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.alexzh.tutorial.notificationdemo.data.DummyData;
@@ -18,18 +12,22 @@ import com.alexzh.tutorial.notificationdemo.data.model.City;
 public class DetailActivity extends AppCompatActivity {
 
     public static final String CITY_ID = "city_id";
+    public static final int INVALID_VALUE = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        TextView textView = findViewById(R.id.textView);
+
+        long cityId = getIntent().getLongExtra(CITY_ID, INVALID_VALUE);
+        if (cityId != INVALID_VALUE) {
+            final City city = DummyData.getCityById(cityId);
+            if (city != null) {
+                textView.setText(city.getDescription());
+            }
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,30 +44,5 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(@NonNull final LayoutInflater inflater,
-                                 @Nullable final ViewGroup container,
-                                 @Nullable final Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            TextView textView = rootView.findViewById(R.id.textView);
-            if (getActivity().getIntent().getLongExtra(CITY_ID, 0) != 0) {
-                final long id = getActivity().getIntent().getLongExtra(CITY_ID, 0);
-                final City city = DummyData.getCityById(id);
-                if (city != null) {
-                    textView.setText(city.getDescription());
-                }
-            }
-            return rootView;
-        }
     }
 }
