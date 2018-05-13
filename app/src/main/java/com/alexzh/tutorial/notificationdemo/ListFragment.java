0fdcdb125e4e1167a5bb9eb49bccdade874fed83
Alex Zhukovich <zhukovich.a@gmail.com>
2018-05-13 14:18:54 +0200

    Updated item_text layout and added DividerItemDecoration to RecyclerView

--- app/src/main/java/com/alexzh/tutorial/notificationdemo/ListFragment.java
+++ app/src/main/java/com/alexzh/tutorial/notificationdemo/ListFragment.java
@@ -5,8 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,12 +42,12 @@ public class ListFragment extends Fragment implements View.OnClickListener {
 
         rootView.findViewById(R.id.send_all_notifications).setOnClickListener(this);
 
-        StaggeredGridLayoutManager mGridLayoutManager =
-                new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
         RecyclerView mRecyclerView = rootView.findViewById(R.id.list);
         mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.addItemDecoration(
+                new DividerItemDecoration(mRecyclerView.getContext(), LinearLayoutManager.VERTICAL));
         CitiesAdapter mAdapter = new CitiesAdapter(mCities, new OnItemClickListener() {
             @Override
             public void onNotificationClick(int position) {
