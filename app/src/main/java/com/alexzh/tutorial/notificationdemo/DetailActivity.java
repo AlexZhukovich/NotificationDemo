6b165358657e1513df475778918b2a5f2fc74151
Alex Zhukovich <zhukovich.a@gmail.com>
2018-05-13 12:57:49 +0200

    Updated activity_detail layout and added loading background to toolbar for the DetailActivity

--- app/src/main/java/com/alexzh/tutorial/notificationdemo/DetailActivity.java
+++ app/src/main/java/com/alexzh/tutorial/notificationdemo/DetailActivity.java
@@ -1,14 +1,21 @@
 package com.alexzh.tutorial.notificationdemo;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.alexzh.tutorial.notificationdemo.data.DummyData;
 import com.alexzh.tutorial.notificationdemo.data.model.City;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -19,33 +26,41 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
         TextView textView = findViewById(R.id.textView);
 
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupToolbar(toolbar);
 
         long cityId = getIntent().getLongExtra(CITY_ID, INVALID_VALUE);
         if (cityId != INVALID_VALUE) {
             final City city = DummyData.getCityById(cityId);
             if (city != null) {
                 textView.setText(city.getDescription());
+                Glide.with(this)
+                        .load(city.getImageURL())
+                        .into(new SimpleTarget<Drawable>() {
+                            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+                            @Override
+                            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                                toolbar.setBackground(resource);
+                            }
+                        });
             }
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
+    private void setupToolbar(@Nullable final Toolbar toolbar) {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("");
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.action_settings:
-                return true;
             case android.R.id.home:
                 onBackPressed();
                 return true;
